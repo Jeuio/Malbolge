d14ccf3fe7d02a8b4f8e0ce88b30abd0ea551a5b
FrostEnte <jeremyohseong@gmail.com>
2021-12-02 16:23:44 +0100

    f

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -5,6 +5,7 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QWidget>
+#include <QPalette>
 
 
 int width;
@@ -13,8 +14,7 @@ int buttonwidth;
 int buttonheight;
 int main(int argc, char** argv)
 {
-    width = 800;
-    height = 600;
+    QPalette pal = QPalette(QColorConstants::Black);
     QApplication app(argc, argv);
     QFont font ("Courier");
     QWidget window;
@@ -22,11 +22,12 @@ int main(int argc, char** argv)
     buttonheight = height / 2;
     buttonwidth = buttonwidth - 50;
     QPushButton *button = new QPushButton(&window);
-    window.setFixedSize(width,height);
     button ->setGeometry(buttonwidth , buttonheight, 100 ,20);
     button->setText("Create Wallet");
     button->setToolTip("This button creates a Wallet");
     button->setFont(font);
+    window.setAutoFillBackground(true);
+    window.setPalette(pal);
     window.show();
     button->show();
     QIcon Qicon ( const QString &name, const QIcon &fallback = QIcon());
